5cd5f5a5369df6908ecae3ca70c2b6d1d4fcfebd
Tom Chapman <tomchapman128@gmail.com>
2023-06-17 11:06:42 +0100

    Add discord.net package

--- Calcu/Program.cs
+++ Calcu/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using Discord.WebSocket;
+
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    private DiscordSocketClient _client;
+
+        public async Task MainAsync()
+    {
+    }
+}
